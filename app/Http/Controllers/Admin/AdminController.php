9b46866089aa93c27d6999e061411b1d1a37d1d1
PYH <2463640340@qq.com>
2017-06-29 21:58:44 +0800

    管理员添加修改

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -22,6 +22,7 @@ class AdminController extends Controller
     public function create()
     {
         //
+        return view("admin.admin.add");
     }
 
     /**
@@ -33,6 +34,29 @@ class AdminController extends Controller
     public function store(Request $request)
     {
         //
+        //表单验证
+        $this->validate($request, [
+            'admin_name' => 'required|max:16',
+            'admin_password' => 'required|max:16|min:6',
+            'admin_phone' => 'required|integer',
+        ]);
+        //判断重复密码
+        if($request->input("admin_password")!=$request->input("repassword")){
+            return back()->with("err","密码和重复密码不一致!");
+            //return "密码和重复密码不一致";
+        }  
+        
+        //获取指定的部分数据
+        $data = $request->only("admin_name","admin_password","admin_phone");
+        $data['admin_password'] = md5($data['admin_password']);
+        $admin_id = Admin::insertGetId($data);
+        
+         if($admin_id>0){
+            //echo "添加成功";
+            return redirect('admin/admin');
+        }else{
+           return back()->with("err","添加失败!");
+        } 
     }
 
     /**
@@ -52,9 +76,11 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($admin_id)
     {
         //
+        $vo = Admin::where("admin_id","=",$admin_id)->first();
+        return view("admin.admin.edit",['vo'=>$vo]);
     }
 
     /**
@@ -64,9 +90,17 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $admin_id)
     {
         //
+        $input = $request->only("admin_name","admin_phone");
+        $m = Admin::where("admin_id",$admin_id)->update($input);
+        if($m){
+            echo "修改用户状态成功!";
+            return redirect("admin/admin");
+        }else{
+            echo "修改用户状态失败!";
+        }
     }
 
     /**
