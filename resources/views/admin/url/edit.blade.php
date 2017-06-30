eb3dfa4ae884ca915efc40f34ecb36e1d2389540
zhanglong_pc@163.com <zhanglong_pc@163.com>
2017-06-30 14:49:31 +0800

    long

--- resources/views/admin/url/edit.blade.php
+++ resources/views/admin/url/edit.blade.php
@@ -1,35 +1,37 @@
-@extends('admin.base')
+锘緻extends('admin.base')
 @section('content')
- <div class="row">
+    <div class="row">
 
-<div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
-	<div class="widget am-cf">
-		<div class="widget-head am-cf">
-			<div class="widget-title am-fl"></div>
-			<div class="widget-function am-fr">
-			<a href="javascript:;" class="am-icon-cog"></a>
-			</div>
-		</div>
-		<div class="widget-body am-fr">
+        <div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
+            <div class="widget am-cf">
+                <div class="widget-head am-cf">
+                    <div class="widget-title am-fl">缂栬緫鍙嬫儏閾炬帴</div>
+                    <div class="widget-function am-fr">
+                        <a href="javascript:;" class="am-icon-cog"></a>
+                    </div>
+                </div>
+                <div class="widget-body am-fr">
 
-			<form class="am-form tpl-form-line-form" action="/admin/url" method="post">
-			<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				 <div class="am-form-group">
-				 <span class="tpl-form-line-small-title">
-			
-					<div class="am-u-sm-9">
-						<input type="text" name="url" class="tpl-form-input" id="user-name" placeholder="请输入链接地址">
-					</div>
-				</div>
-			<div class="am-form-group">
-			<div class="am-u-sm-12 am-u-sm-push-12">
-				<input type="submit" class="am-btn am-btn-primary tpl-btn-bg-color-success" value="提交"/>
-			</div>
-			</div>
-			</form>
-			
-		
-		</div>
-	</div>
-</div>
+                    <form class="am-form tpl-form-border-form tpl-form-border-br" action="/admin/url/{{$v->id}}" method="post">
+                        <input type="hidden" name="_method" value="put">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <div class="am-form-group">
+                            <label for="user-name" class="am-u-sm-3 am-form-label" name="url" value="{{$v->url}}">鍙嬫儏閾炬帴</label>
+                            <div class="am-u-sm-9">
+                                <input type="text" style="width:200px" class="tpl-form-input" name="url" id="user-name">
+                            </div>
+                        </div>
+                       
+                        <div class="am-form-group">
+                            <div class="am-u-sm-9 am-u-sm-push-3">
+                                <input type="submit" class="am-btn am-btn-primary tpl-btn-bg-color-success " value="鎻愪氦" />
+                            </div>
+                        </div>
+                    </form>
+                    <br/>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+		
