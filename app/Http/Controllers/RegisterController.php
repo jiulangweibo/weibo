60dda63cc80112b953d4c3b39ea72d7cb37e5b2a
赵伊飞 <1071681710@qq.com>
2017-07-05 22:26:03 +0800

    注册

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Model\Register;
 use \App\Model\Userinfo;
-use Illuminate\Database\Seeder\UsersTableSeeder;
+//use Illuminate\Database\Seeder\UsersTableSeeder;
 
 class RegisterController extends Controller
 {
@@ -31,21 +31,35 @@ class RegisterController extends Controller
 	   public function store(Request $request)
     {
         //表单验证
-      
+		
 
         //获取指定的部分数据
-        $data = $request->only('phone','password','nickname');
-		$data['password'] = md5($data['password']);
-        $data['register_time'] = date("Y-m-d H:i:s",time());
-        $id = Register::insertGetId($data);
-        $data['status'] = 0;
-        
-        $userinfo = Userinfo::insertGetId($data);
-        
-        if($id>0){
-            return redirect('/');
-        }else{
-           return back()->with("err","添加失败!");
-        }
+       /* $phone = $request->only('phone');
+ 
+        $nickname = $request->only('nickname');
+		$register =Register::where("phone",$phone)->first();
+		$nickname =Register::where("nickname",$nickname)->first(); */
+		//判断账号是否存在
+		
+			
+			$data = $request->only('phone','password','nickname');
+			$data['password'] = md5($data['password']);
+			$data['register_time'] = date("Y-m-d H:i:s",time());
+			
+			$id = Register::insertGetId($data);
+			//return($id);
+			//同时添加数据到userinfo表中 并且 userinfo表中的user_id = register中的主键id;
+			$data['user_id']=$id;
+			//return($data);
+			$dd = Userinfo::insertGetId($data);
+			
+			if($dd>0 && $id>0){
+				return redirect('/');
+			}else{
+			   return back()->with("err","添加失败!");
+			}
+		
+		
+	
     }
 }
