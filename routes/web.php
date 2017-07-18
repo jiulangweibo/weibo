dd2bdbae81702cb70bc2c5b240a69d234cf494c1
PYH <2463640340@qq.com>
2017-07-18 14:29:03 +0800

    登陆后前台搜索

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::resource('/account',"Home\AccountController");		  //执行加载账号设
 
 Route::get('/follow',"Home\FollowController@index");      //加载关注主页
 Route::get('/fans',"Home\FansController@index");      //加载粉丝主页
-Route::get('/indexs/search',"Home\IndexsController@search");      //加载搜索页
+Route::get('/search',"Home\IndexsController@search");      //加载搜索页
 
 });
 Route::post('/avatar/upload',"Home\AvatarController@uploadFile");	
