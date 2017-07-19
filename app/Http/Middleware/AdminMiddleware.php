f4ceec68d4cc83316527ebe93d72c4c760cd0c6e
赵伊飞 <1071681710@qq.com>
2017-07-19 08:43:02 +0800

    Update AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -1,51 +1,46 @@
 <?php
+namespace App\Http\Controllers\Admin;
 
-namespace App\Http\Middleware;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Gregwar\Captcha\CaptchaBuilder;
 
-use Closure;
-
-class AdminMiddleware
+class LoginController extends Controller
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        //判断会员是否没有登录
-        if(!$request->session()->has('adminuser')){
-            return redirect('admin/login');
-        }
-		  return $next($request);
-        //return $next($request);//继续往后走
-        //dd(session("adminuser"));
-       //  判断是否是超级用户
-
-        // if(session("adminuser")->admin_id==1){
-        //     return $next($request);
-        // }
-      //  判断权限   
-       //dd(session("nodelist"));
-        $nodelist = session("nodelist");
-        //dd($nodelist);//dd($nodelist);
-
-        foreach($nodelist as $k=>$v){
-            //判断权限
-            //dd($request);
-            foreach ($v as $key => $value) {
-              if($request->is($value['url']) && $request->isMethod($value['method'])){
-                    return $next($request);
-                }
-            }
-                
-        }
-        return back()->with("err","抱歉你没有此操作权限!");  
+   //加载登录模板
+   public function login()
+   {
+       return view("admin.login");
+   }
+   
+   //执行用户登录
+  public function doLogin(Request $request)
+   {
 
 
         
-    }
-
+        //执行登陆判断
+        $admin_name = $request->input("admin_name");
+        $admin_password = $request->input("admin_password");
+        //获取对应用户信息
+        $user = \DB::table("admin")->where("admin_name",$admin_name)->first();
+        if(!empty($user)){
+            //判断密码
+            if(md5($admin_password)==$user->admin_password){
+                //存储session跳转页面
+                session()->push("adminuser",$user);
+                return redirect("admin");
+                //echo "测试成功!";
+            }
+        }
+        return back()->with("msg","账号或密码错误！");
+   }
+ 
+   
+   //执行退出
+   public function logout(Request $request)
+   {
+       $request->session()->forget('adminuser');
+       return redirect("admin/login");
+   }
 }
